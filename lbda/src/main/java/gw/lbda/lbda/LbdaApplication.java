82c83e7c678c03cdbce4b6fbcccfdfcd003afa9c
MijuZS <eliseo.fernandezmonroy@alum.uca.es>
2021-10-19 17:36:50 +0200

    init commit

---
+++ lbda/src/main/java/gw/lbda/lbda/LbdaApplication.java
@@ -0,0 +1,13 @@
+package gw.lbda.lbda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbdaApplication.class, args);
+	}
+
+}
---
+++ lbda/src/test/java/gw/lbda/lbda/LbdaApplicationTests.java
@@ -0,0 +1,13 @@
+package gw.lbda.lbda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
